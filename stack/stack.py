de74444dc42cbf66283a8bc2460f026df2e4e311
Aleksei-Zaichenko <aleksei.zaichenko@cix.csi.cuny.edu>
2020-07-13 17:08:24 -0400

    Adds Stack class definition to stack.py

--- stack/stack.py
+++ stack/stack.py
@@ -1,3 +1,5 @@
+from singly_linked_list import LinkedList
+
 """
 A stack is a data structure whose primary purpose is to store and
 return elements in Last In First Out order. 
@@ -11,16 +13,47 @@ return elements in Last In First Out order.
    implementing a Stack?
 """
 
+# 1
+# Stack using a list
+# class Stack:
+#     def __init__(self):
+#         self.size = 0
+#         self.storage = []
+
+#     def __len__(self):
+#         return len(self.storage)
+
+#     def push(self, value):
+#         self.storage.append(value)
+#         self.size += 1
+
+#     def pop(self):
+#         if(len(self.storage) > 0):
+#             self.size -= 1
+#             return self.storage.pop()
+
+# 2
+# Stack using a LinkedList
 class Stack:
     def __init__(self):
         self.size = 0
-        # self.storage = ?
+        self.storage = LinkedList()
 
     def __len__(self):
-        pass
+        return self.size
 
     def push(self, value):
-        pass
+        self.size += 1
+        self.storage.add_to_tail(value)
 
     def pop(self):
-        pass
+        if(self.size > 0):
+            self.size -= 1
+            return self.storage.remove_tail()
+
+# 3
+# I think the difference betweeen  using a list and a linked list is that
+# when a list is being used a computer sequantially allocate memory for given values
+# and Stack as a data structure is not implemented
+# whereas, when linked link is used to create a Stack, memory will be allocated randomly
+# and Stack will not need to recreate itself if it reaches occupaid memory slot
